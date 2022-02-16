67ea2a8cba158b91be80812ce6c8d2f95e3705ca
Jascha030 <contact@jaschavanaalst.nl>
2022-02-16 22:09:53 +0100

    ✅ More tests while also being less test

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 
 internal class MainKtTest {
-    private val testArray: Array<String> = arrayOf("test1", "test2", "test3")
+    private val testArrayOfThree: Array<String> = arrayOf("test1", "test2", "test3")
+    private val testArrayOfOne: Array<String> = arrayOf("test1")
 
     @Test
-    fun shift() {
-        val expected = arrayOf("test2", "test3")
-
-        assertArrayEquals(expected, testArray.shifted())
-    }
+    fun shifted() = assertArrayEquals(arrayOf("test2", "test3"), testArrayOfThree.shifted())
+    
+    @Test
+    fun shiftedWithSingleValue() = assertArrayEquals(emptyArray<String>(), testArrayOfOne.shifted())
 }
