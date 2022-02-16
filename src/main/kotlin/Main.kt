2bcdff13108e0f63ee194c06207c88f2b3a387d2
Jascha030 <contact@jaschavanaalst.nl>
2022-02-16 22:12:11 +0100

    🎨 Makes you wonder, is this actually helpful in the long run? Guess one of the goals of kotlin is doing "java" by writing "j".

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,16 @@
-fun main(args: Array<String>) {
-}
-
-fun <T> Array<T>.shifted(): Array<T> = copyOfRange(indexOf(first()) + 1, size)
+fun main(args: Array<String>) = dispatchCommand(args)
 
 private val availableCommands = listOf(::init, ::open, ::help).associateBy { it.name }
 
-private fun dispatchCommandFromArgument(command: String, args: Array<String>?) =
-    availableCommands[command]?.invoke(args) ?: error("Invalid command: $command")
+fun <T> Array<T>.shifted(): Array<T> = copyOfRange(indexOf(first()) + 1, size)
+
+private fun dispatchCommand(args: Array<String>) {
+    val command = args.first()
+
+    availableCommands[command]
+        ?.invoke(args.shifted())
+        ?: error("Invalid command: $command")
+}
 
 fun init(args: Array<String>?) {}
 
