53f13ec2d3e3875c73357f02e52d4619ff39dc09
Jascha030 <contact@jaschavanaalst.nl>
2022-02-16 21:39:15 +0100

    🚧 Add array extension to copy with first value shifted off (trying dingen out).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>) {
 }
 
+fun <T> Array<T>.shifted(): Array<T> {
+    return this.copyOfRange<T>(this.indexOf(this.first()) + 1, this.size)
+}
+
 private val availableCommands = listOf(::init, ::open, ::help).associateBy { it.name }
 
 private fun dispatchCommandFromArgument(command: String, args: Array<String>?) =
